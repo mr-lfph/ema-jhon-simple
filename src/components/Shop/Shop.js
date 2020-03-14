174a3af6cdfb79a0217355f5470095d7cead19f5
mr-lfph <mr-lfph@gmail.com>
2020-03-14 16:00:32 +0800

    useStateProblem

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+
+const Shop = () => {
+    useState
+    const first10= fakeData.slice(0,10);
+    const [products,setProducts]=useState(first10);
+
+    return (
+        <div>
+            <h1>This is Shop</h1>
+            <h3>{products.length}</h3>
+         <ul>{
+             products.map(product=><li>{products.name}</li> )
+             }
+           </ul>
+        </div>
+    );
+};
+
+export default Shop;
